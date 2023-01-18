80556d4f1e2d5f205941acedf25cfcc40b324f78
ramanassif <rama.nassif.4@gmail.com>
2023-01-18 13:45:30 +0300

    Create custom note item

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        top: 24,
+        bottom: 24,
+        left: 16,
+      ),
+      decoration: BoxDecoration(
+        color: const Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                'Add your tip that help the others',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Text(
+              DateFormat('dd-MM-yyyy').format(DateTime.now()).toString(),
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.5),
+                fontSize: 14,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
