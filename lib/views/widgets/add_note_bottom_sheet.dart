be60a690a31ca45e155e24b10be1d594af18f013
ramanassif <rama.nassif.4@gmail.com>
2023-01-19 15:13:42 +0300

    Add button add note to bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -9,20 +10,44 @@ class AddNoteBottomSheet extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
-        children: const [
-          SizedBox(
+        children: [
+          const SizedBox(
             height: 30,
           ),
-          CustomTextField(
+          const CustomTextField(
             hintText: 'Title',
             maxLines: 1,
           ),
-          SizedBox(
+          const SizedBox(
             height: 16,
           ),
-          CustomTextField(
+          const CustomTextField(
             hintText: 'Content',
             maxLines: 4,
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(kPrimaryColor),
+                padding: MaterialStateProperty.all(
+                  const EdgeInsets.symmetric(horizontal: 100),
+                ),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
           ),
         ],
       ),
