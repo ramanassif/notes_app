cb801f1d81aac78063312af4112bc3b1170b53a5
ramanassif <rama.nassif.4@gmail.com>
2023-01-19 13:58:05 +0300

    Create custom text field

---
+++ lib/views/widgets/custom_text_field.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'Title',
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16.0),
+      borderSide: BorderSide(
+        color: color ?? Colors.white,
+      ),
+    );
+  }
+}
