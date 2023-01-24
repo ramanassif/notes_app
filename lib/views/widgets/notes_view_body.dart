7df1f55bfab6d12e140d83cbad5bc5e890b9832e
ramanassif <rama.nassif.4@gmail.com>
2023-01-24 15:34:55 +0300

    Refactor code and display notes part one

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes/notes_cubit.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
-import 'package:notes_app/views/widgets/custom_note_item.dart';
 import 'package:notes_app/views/widgets/notes_list_view.dart';
 
-class NotesViewBody extends StatelessWidget {
+class NotesViewBody extends StatefulWidget {
   const NotesViewBody({Key? key}) : super(key: key);
 
+  @override
+  State<NotesViewBody> createState() => _NotesViewBodyState();
+}
+
+class _NotesViewBodyState extends State<NotesViewBody> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,10 +21,19 @@ class NotesViewBody extends StatelessWidget {
           SizedBox(
             height: 8,
           ),
-          CustomAppBar(title: 'Notes',icon: Icons.search,),
+          CustomAppBar(
+            title: 'Notes',
+            icon: Icons.search,
+          ),
           Expanded(child: NotesListView()),
         ],
       ),
     );
+  }
+
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
   }
 }
