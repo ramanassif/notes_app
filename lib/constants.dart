54c4f0f102c81f1b06e9cf30bfe736782e765443
ramanassif <rama.nassif.4@gmail.com>
2023-01-25 15:19:08 +0300

    Complete change color for new note

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xff62FCD7);
+const kPrimaryColor = Color(0xffe29578);
 const kNotesBox = 'notes_box';
 List<Color> colors = const [
   Color(0xff586BA4),
-  Color(0xff324376),
+  Color(0xff98c1d9),
   Color(0xffBDBEA9),
   Color(0xff8DB38B),
   Color(0xff56876D),
   Color(0xff04724D),
+  Color(0xff419D78),
   Color(0xffF5DD90),
+  Color(0xffffd670),
   Color(0xffF68E5F),
   Color(0xffF76C5E),
+  Color(0xffe56b6f),
   Color(0xffD2AB99),
+  Color(0xffFFDBB5),
+  Color(0xffE0A458),
 ];
--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/views/widgets/color_item.dart';
 
 class ColorsListView extends StatefulWidget {
@@ -23,6 +25,7 @@ class _ColorsListViewState extends State<ColorsListView> {
           return GestureDetector(
             onTap: () {
               currentIndex = index;
+              BlocProvider.of<AddNoteCubit>(context).color = colors[index];
               setState(() {});
             },
             child: ColorItem(
